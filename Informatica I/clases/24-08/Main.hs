cef76760cf3ff5a4be9e8030352bb84431aac14f
Gregorio0505 <josegregoriocoronelcolombogmail.com>
2021-09-01 08:58:07 -0600

    master

--- Informatica I/clases/24-08/Main.hs
+++ Informatica I/clases/24-08/Main.hs
@@ -7,7 +7,9 @@ import Prelude (
     Show,
     (-),
     (+),
-    undefined
+    undefined,
+    (*),
+    div
     )
 
 -- 1 -> (2 -> (3 -> (4 -> _ ) ) )
@@ -19,10 +21,38 @@ import Prelude (
 -- 2) agregar n al final
 -- 3) (3 -> 2 -> 1 -> _)
 
+data Nat = Cero | Succ Nat
+
+-- 1 := Succ Cero
+-- 2 := Succ (Succ Cero)
+
+-- Int := (-2^63 - 2^63)
 data Lista = Cons Int Lista | Nil deriving Show
 
+-- range 3
+-- | n = 3
+-- = Cons 3 (range (3 - 1)) = Cons 3 (range 2)
+-- | n = 2
+-- = Cons 3 (Cons 2 (Range (2 - 1))) = Cons 3 (Cons 2 (Range 1))
+-- | n = 1
+-- = Cons 3 (Cons 2 (Cons 1 (Range (1 - 1)))) = Cons 3 (Cons 2 (Cons 1 (Range 0)))
+-- | n = 0
+-- = Cons 3 (Cons 2 (Cons 1 Nil))
 range 0 = Nil
 range n = Cons n (range (n - 1))
+
+--Range' 3
+-- | n = 3
+-- = Cons 3 (range' (3 + 1)) = Cons 3 (range' 4)
+-- | n = 4
+-- = Cons 3 (Cons 4 (range ' (4 + 1))) = Cons 3 (Cons 4 (range ' 5))
+
+range' 0 = Nil
+range' n = Cons n (range' (n + 1))
+
+tomar _ Nil = Nil
+tomar 0 _ = Nil
+tomar n (Cons x xs) = Cons x (tomar (n - 1) xs)
 
 -- agregarFinal 4 (Cons 1 (Cons 2 (Cons 3 Nil)))
 -- | x = 4, n = 1, ns = (Cons 2 (Cons 3 Nil))
