04a7e65827a3397ba24873e30e9c1791952d83d1
mnrescobar <mescobarc@unis.edu.gt>
2021-09-07 14:37:53 -0600

    Entrega de LAb #4

--- Informatica I/laboratorios/laboratorio 2/Main.hs
+++ Informatica I/laboratorios/laboratorio 2/Main.hs
@@ -7,6 +7,3 @@ data Natural = Cero | Succ Natural
 Cero + m = m
 n + Cero = n
 n + (Succ a) = Succ (n + a)
-
-factorial Cero = Succ Cero
-factorial (Succ a) = -- multiplicar (Succ a) por el factorial de a
