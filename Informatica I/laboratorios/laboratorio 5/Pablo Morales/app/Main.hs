3625e2f3447499c2a1b2765e91594654f8a5fcae
pmoralesm355 <pablo.moralesm355@gmail.com>
2021-09-11 15:59:11 -0600

    lab5

--- Informatica I/laboratorios/laboratorio 5/Pablo Morales/app/Main.hs
+++ Informatica I/laboratorios/laboratorio 5/Pablo Morales/app/Main.hs
@@ -2,13 +2,13 @@
 
 module Main where
 
-import Prelude (Int, Float, Bool(..), Show, (+), (-), (==), (||), (*), (>), (&&), (**), floor, fst, snd, div, mod, undefined)
+import Prelude (Int, Float, Bool(..), Show, (+), (-), (==), (||), (*), (>), (&&), (**), floor, fst, snd, div, mod, drop, take, undefined)
 
 data Lista = Nil | Cons Int Lista deriving Show
 
 fold :: (estado -> Int -> estado) -> estado -> Lista -> estado
 fold agg cero Nil = cero
-fold agg cero (Cons x xs) =
+fold agg cero (Cons x xs) = 
     agg (fold agg cero xs) x
 
 -- Problema 1: 
@@ -19,8 +19,26 @@ fold agg cero (Cons x xs) =
 
 pushAgregador estado x = Cons x estado
 
+-- pushBack 3 (Cons 1 (Cons 2 Nil))
+-- |pushBack: x = 3, xs = (Cons 1 (Cons 2 Nil))|
+-- = fold: pushAgregador (Cons 3 Nil) (Cons 1 (Cons 2 Nil))
+-- |fold: cons = pushAgregador, nil = (Cons 3 Nil), x = 1, xs = (Cons 2 Nil)
+-- = pushAgregador (fold PushAgregador (Cons 3 Nil) (Cons 2 Nil)) 1
+-- |fold: cons = pushAgregador, nil = Cons 3 Nil, x = 2, xs = Nil|
+-- = pushAgregador (pushAgregador (fold pushAgregador (Cons 3 Nil) Nil) 2) 1 
+-- |fold: cons = pushAgregador, nil = (Cons 3 Nil)|
+-- = pushAgregador (pushAgregador (Cons 3 Nil) 2) 1
+-- |pushAgregador: estado = Cons 3 Nil, x = 2|
+-- = pushAgregador (Cons 2 (Cons 3 Nil)) 1
+-- |pushAgregador: estado = (Cons 2 (Cons 3 Nil)), x = 1|
+-- = Cons 1 (Cons 2 (Cons 3 Nil))
+
 pushBack x xs =
     fold pushAgregador (Cons x Nil) xs
+
+
+
+
 
 -- Problema 2:
 -- Utilizar la funcion "fold" para definir
@@ -30,6 +48,25 @@ pushBack x xs =
 -- que aparecen en la lista.
 -- Puede basarse en la funcion "drop" para
 -- su implementacion.
+
+take _ Nil = Nil
+take 0 _ = Nil
+take n (Cons x xs) = Cons x (take' (n - 1) xs)
+ 
+takeAgg (resultado, 0) x = (resultado, 0)
+takeAgg (resultado, n) x = (pushBack x resultado, n - 1)
+
+takeAux n xs = fold takeAgg (Nil, n) xs        
+
+reverse' Nil = Nil
+reverse' (Cons x xs) = pushBack x (reverse xs)
+
+take' n xs = (fst (takeAux n (reverse' xs)))
+
+
+
+
+
 
 -- Problema 3:
 -- Utilizar la funcion "fold" para definir
@@ -42,6 +79,30 @@ pushBack x xs =
 -- elem 0 (Cons 1 (Cons 2 (Cons 3 Nil))) == 1
 -- elem 1 (Cons 1 (Cons 2 (Cons 3 Nil))) == 2
 
+-- elem i Nil = 0
+-- elem 0 (Cons x xs) = x
+-- elem i (Cons x xs) = elem (i - 1) xs
+-- elemAgg (Cons x Nil) = x
+
+elemAgg (Cons x Nil) = x 
+
+elemAgregador (resultado, i) x = 
+    if i == 0 
+        then m 
+        else n 
+
+    where 
+        m = (Cons x Nil, i - 1)
+        n = (resultado, i - 1) 
+
+elemAux i xs = fold (elemAgregador) (Nil, i) xs
+
+elem i xs = elemAgg (fst (elemAux i (reverse xs)))
+
+
+
+
+
 -- Problema 4:
 -- Utilizar la funcion "fold" para definir
 -- la funcion "update". Esta funcion debe
@@ -52,12 +113,54 @@ pushBack x xs =
 -- update 0 42 (Cons 1 (Cons 2 (Cons 3 Nil))) == (Cons 42 (Cons 2 (Cons 3 Nil)))
 -- update 2 42 (Cons 1 (Cons 2 (Cons 3 Nil))) == (Cons 1 (Cons 2 (Cons 42 Nil)))
 
+
+--Definición fold 
+
+fold' agg cero Nil = cero
+fold' agg cero (Cons x xs) =
+    agg (fold agg cero xs) x
+
+--Definición update
+updateAux (resultado, i) v xs = 
+    if i == v 
+        then (Cons i Nil, i)
+        else (resultado, i)
+
+--updateAux i v xs = fold (updateAgregador) (i, v) xs 
+
+
+
+
+
+
 -- Problema 5:
 -- Utilizar la funcion "fold" para definir
 -- la funcion "map". En otras palabras,
 -- provea una definicion alterna de la
 -- funcion "map" que este definida en
 -- terminos de la funcion fold.
+
+
+--fold agg cero Nil = cero
+--fold agg cero (Cons x xs) =
+    --agg (fold agg cero xs) x
+
+-- Definición: función map alterna = map'
+map :: (Int -> Int) -> Lista -> Lista
+map _ Nil = Nil
+map n (Cons x xs) = Cons (n x) (map n xs)
+
+map' :: Int -> (Int -> t -> b) -> Lista -> t -> (Lista, b)
+map' n estado x xs = (Cons n x, estado n xs)
+
+
+foldm :: ((t1 -> t2) -> t1 -> t2) -> t3 -> t1 -> t1 -> t2
+foldm map' estado x xs = map' (foldm map' estado xs) x 
+
+
+
+
+
 
 -- Ejercicios de repaso:
 -- A continuacion se proveen ejercicios
@@ -129,6 +232,7 @@ reverse Nil = Nil
 reverse (Cons x xs) = pushBack x (reverse xs)
 
 
+convertirAListaAux :: Int -> Lista
 convertirAListaAux 0 = Nil
 convertirAListaAux n = Cons (mod n 10) (convertirAListaAux (div n 10)) 
 
